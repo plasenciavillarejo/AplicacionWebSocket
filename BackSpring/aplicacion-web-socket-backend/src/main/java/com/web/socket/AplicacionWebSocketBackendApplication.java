15fdd0f14f23aaadf43bbfe3e8aa3f6f5a4e008b
Jose Plasencia <jose.plasencia@externos.correo.gob.es>
2024-03-09 20:20:46 +0100

    Web Socket

---
+++ BackSpring/aplicacion-web-socket-backend/src/main/java/com/web/socket/AplicacionWebSocketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.web.socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionWebSocketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionWebSocketBackendApplication.class, args);
+	}
+
+}
