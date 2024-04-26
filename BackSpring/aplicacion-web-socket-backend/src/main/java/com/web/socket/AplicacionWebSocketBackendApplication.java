bb5921ac7423e383196fbc29f17f544960369727
joseplasenciavillarejo <plasenciavillarejo@gmail.com>
2024-04-26 22:08:37 +0200

    subida

--- BackSpring/aplicacion-web-socket-backend/src/main/java/com/web/socket/AplicacionWebSocketBackendApplication.java
+++ BackSpring/aplicacion-web-socket-backend/src/main/java/com/web/socket/AplicacionWebSocketBackendApplication.java
@@ -2,8 +2,9 @@ package com.web.socket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class AplicacionWebSocketBackendApplication {
 
 	public static void main(String[] args) {
--- BackSpring/aplicacion-web-socket-backend/src/main/java/com/web/socket/config/WebSocketConfig.java
+++ BackSpring/aplicacion-web-socket-backend/src/main/java/com/web/socket/config/WebSocketConfig.java
@@ -1,7 +1,10 @@
 package com.web.socket.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -11,24 +14,47 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-  @Override
-  public void registerStompEndpoints(StompEndpointRegistry registry) {
-    // Aquí se le indica el endpoint que se quiere agregar para conectar desde angular a este servidor websocket
-    registry.addEndpoint("/chat-websocket")
-    // Habilitamos el origin de angular
-    .setAllowedOriginPatterns("http://localhost:4200")
-    /* habilitamos la librería que por debajo va a utilizar SockJs() que nos permite utilizar el protocolo http para conectar con angular
-     de lo contrario nos obliga a conectar con la api nativa de html5 pero este solo soparte el protocolo ws */
-    .withSockJS();
-  }
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		// Aquí se le indica el endpoint que se quiere agregar para conectar desde
+		// angular a este servidor websocket
+		registry.addEndpoint("/chat-websocket")
+				// Habilitamos el origin de angular
+				.setAllowedOriginPatterns("http://localhost:4200")
+				/*
+				 * habilitamos la librería que por debajo va a utilizar SockJs() que nos permite
+				 * utilizar el protocolo http para conectar con angular de lo contrario nos
+				 * obliga a conectar con la api nativa de html5 pero este solo soparte el
+				 * protocolo ws
+				 */
+				.withSockJS();
+	}
   
-  @Override
-  public void configureMessageBroker(MessageBrokerRegistry registry) {
-    /* Este es un prefijo, sirve para los nombres de eventos, cuando el servidor emite un mensaje debe notificar a todos los clientes mediante el nombre del evento,
-    par que los cliente que este subscritos este avisados del evento */
-    registry.enableSimpleBroker("/chat/");
-    // Prefijo de el destino cuando publicamos un mensaje, enviamos nuestro payload hacia nuestro websocket
-    registry.setApplicationDestinationPrefixes("/app");
-  }
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		/*
+		 * Este es un prefijo, sirve para los nombres de eventos, cuando el servidor
+		 * emite un mensaje debe notificar a todos los clientes mediante el nombre del
+		 * evento, par que los cliente que este subscritos este avisados del evento
+		 */
+		registry.enableSimpleBroker("/chat/");
+		// Prefijo de el destino cuando publicamos un mensaje, enviamos nuestro payload
+		// hacia nuestro websocket
+		registry.setApplicationDestinationPrefixes("/app");
+	}
   
+	@Bean
+	WebMvcConfigurer corsConfig() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/ws/**")
+				.allowedMethods("GET", "POST", "PUT", "DELETE")
+				.allowedHeaders("*")
+				.allowedOrigins("http://localhost:4200")
+				.allowedOriginPatterns("*");
+			}
+		};
+	}
+
 }
